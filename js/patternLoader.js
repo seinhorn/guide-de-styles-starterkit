a2ccff68cf43ea8b1ca80682da6fe92b8d82102e
acariou <Arnaud*4>
2013-08-06 13:35:21 +0200

    Ajout des principaux modules

---
+++ js/patternLoader.js
@@ -0,0 +1,37 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 13/05/13
+ * Time: 12:28
+ * To change this template use File | Settings | File Templates.
+ */
+;(function($){
+	$.fn.loadPattern = function (opts, myCcallback) {
+		// comment on valide que le type est objet
+		var settings = {
+			file: null
+			, loadContext: null
+			, context: null
+		};
+
+		var options = $.extend(settings,opts);
+
+		return this.each(function(){
+
+			var masques = $(this).load(
+				options.file+" "+options.loadContext,
+				function(response, status, xhr){
+					if (status == "error") {
+						var msg = "Impossible de charger le fichier" + xhr.status + " " + xhr.statusText;
+						$('<div id="error"/>').insertAfter(this).html(msg);
+					} else {
+						$(this).find("pre.htmlCode").snippet("html",{style:"acid",transparent:true});
+						if (typeof myCcallback === 'function') {
+							myCcallback.call(this);
+						}
+					}
+				}
+			);
+		});
+
+	};
+})(jQuery);
