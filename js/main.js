30adf753bf661552f45414fee05fa15d1d24d2a9
acariou <Arnaud*4>
2013-10-29 00:24:00 +0100

    Merge commit '8fc570202eebaf4310e52222925cadbd758610fe'; tag 'base'
    Conflicts:
    	index.html
    	js/patternLoader.js
    	patterns/charte/typography.html
    	sass/modules/_tabs.scss
    	sass/tests/app.scss

--- js/main.js
+++ js/main.js
@@ -2,18 +2,15 @@
  * Configuration des dependances qui ne sont pas des modules AMD.
  */
 requirejs.config({
+
 	paths: {
-		'jquery': 'vendor/jquery/1.8.3/jquery'
-		,'h5f': 'vendor/h5f/h5f.min'
-		,'snippet': 'vendor/snippet/jquery.snippet.min'
-		,'handlebars': 'vendor/handlebars/1.0.0/handlebars'
-		,'foundation': 'vendor/foundation/foundation'
-		,'foundation.section': 'vendor/foundation/foundation.section'
+		'jquery': 		'vendor/jquery/1.8.3/jquery'
+		,'h5f': 		'vendor/h5f/h5f.min'
+		,'snippet': 	'vendor/snippet/jquery.snippet.min'
+		,'handlebars': 	'vendor/handlebars/1.0.0/handlebars'
 	},
 	shim: {
 		'jquery': { exports: '$' }
-		,'foundation': { deps: ['jquery'] }
-		,'foundation.section': { deps: ['foundation'] }
 		,'snippet': { deps: ['jquery'] }
 		,'affichePanelPage': { deps: ['jquery'] }
 		,'navigation': { deps: ['jquery'] }
@@ -26,7 +23,8 @@ requirejs.config({
 
 require(
 	[
-		'handlebars'
+		'setting'
+		,'handlebars'
 		,'snippet'
 		,'alertBoxes'
 		,'patternLoader'
@@ -34,191 +32,64 @@ require(
 		,'h5f'
 		,'affichePanelPage'
 		,'navigation'
-		,'foundation'
-		,'foundation.section'
-	], function () { //mustache
+	],
+	function (setting) {
 
-		// permet de savoir quel media queries est utilise dans les css
-		var bodyViewportClasses = function() {
+		$(function(){
+			// permet de savoir quel media queries est utilise dans les css
+			// les classes sont ajoutes a la balise body
+			var bodyViewportClasses = function() {
 
-			var nav_display = $('#mdetect').css('display');
-			if (nav_display === 'block') {
-				$("body")
-					.removeClass("big-screen")
-					.addClass("small-screen");
-			}
-			if (nav_display === 'none') {
-				$("body")
-					.removeClass("small-screen")
-					.addClass("big-screen");
-			}
-		}
+				var nav_display = $('#mdetect').css('display');
+				if (nav_display === 'block') $("body").removeClass("big-screen").addClass("small-screen");
+				if (nav_display === 'none') $("body").removeClass("small-screen").addClass("big-screen");
+			};
 
-		$(window).resize(function(){
+			$(window).resize(function(){ bodyViewportClasses(); });
+
 			bodyViewportClasses();
-		});
 
-		bodyViewportClasses();
+			// Construction du menu de navigation
+			var tplNav = $('#tpl-nav').html();
+			var output = Handlebars.compile(tplNav);
 
-		//$('nav[role="navigation"]').affichePanelPage().navigation();
+			var $navigation = $('nav[role="navigation"]');
+			$navigation.append(output(setting));
 
-		// Charte ou intro a voir
-		$("#color-swatches").patternLoader(
-			{
-				file:'patterns/charte/color-swatches.html',
-				loadContext: '.page-color-swatches'
-			});
-		$("#fonts").patternLoader(
-			{
-				file:'patterns/charte/fonts.html',
-				loadContext: '.page-fonts'
-			});
-		// Typography
-		$("#typography").patternLoader(
-			{
-				file:'patterns/charte/typography.html',
-				loadContext: '.page-typography',
-				code: true
-			},
-			function(){
-				$("pre.htmlCode").snippet("html",{style:"acid",transparent:true});
-			}
-		);
-		// Layout
-		$("#grid").patternLoader({file:'patterns/layout/grid.html', loadContext: '.page-grid',code:true});
-		$("#grid-block").patternLoader({file:'patterns/layout/grid-block.html', loadContext: '.page-grid-block',code:true});
-		$("#panel").patternLoader({file:'patterns/layout/panel.html', loadContext: '.page-panel',code:true});
-		// $("#off-canvas").patternLoader({file:'patterns/layout/off-canvas.html', loadContext: '.page-off-canvas'});
-		$("#boutons").patternLoader({file:'patterns/ui/boutons.html', loadContext: '.page-boutons',code:true});
-		// $("#tooltips").patternLoader({file:'patterns/ui/tooltips.html', loadContext: '.page-tooltips'});
-		// $("#nav").patternLoader({file:'patterns/ui/nav.html', loadContext: '.page-nav'});
-		// Formulaires
-		$("#form").patternLoader({file:'patterns/forms/forms.html', loadContext: '.page-form',code:true});
-		$("#alert-boxes").patternLoader(
-			{
-				file:'patterns/forms/alert-boxes.html',
-				loadContext: '.page-alert-boxes',
-				code:true
-			},
-			function(){
-				$.alertBoxes();
-			}
-		);
-		// Effets
-		$("#masques").patternLoader({file:'patterns/fx/masques.html', loadContext: '.masque',code:true});
+			// Construction du corps de la page
+			var $contener = $('section[role=main]');
+			// On boucle sur l'objet de configuration
+			$.each(setting.panelPage, function(i) {
 
-		$("pre.htmlCode").snippet("html",{style:"acid",transparent:true});
+				var self = this;
 
-		$(document).foundation();
+				$contener.append('<div class="panel-page" id="' + self.id + '"></div>');
 
-		var guiStructure = {
-			panelPage: [
-				{
-					id: 'charte'
-					,title: 'Charte'
-					,hash: ''
-					,patterns: [
-						{
-							id: 'color-swatches',
-							title: 'Code couleur',
-							hash: '',
-							src: 'patterns/charte/color-swatches.html',
-							loadContext: '.page-color-swatches'
-						}
-						,{
-							id: 'fonts',
-							title: 'Polices',
-							hash: '',
-							src: 'patterns/charte/fonts.html',
-							loadContext: '.page-fonts'
-						}
-					]
-				},
-				{
-					id: 'typography',
-					title: 'Typography',
-					hash: '',
-					patterns: [
-						{
-							id: 'titletypo'
-							,title:'Les titres'
-							,hash: ''
-							,OuvertureSousMenu: 'right'
-							,patterns: [
-								{id: 'h1',title: 'H1',hash: '',src: 'patterns/typography/h1.html',loadContext: '.page-h1'}
-								,{id: 'h2',title: 'H2',hash: '',src: 'patterns/typography/h2.html',loadContext: '.page-h2'}
-								,{id: 'h3',title: 'H3',hash: '',src: 'patterns/typography/h3.html',loadContext: '.page-h3'}
-								,{id: 'h4',title: 'H4',hash: '',src: 'patterns/typography/h4.html',loadContext: '.page-h4'}
-								,{id: 'h5',title: 'H5',hash: '',src: 'patterns/typography/h5.html',loadContext: '.page-h5'}
-								,{id: 'h6',title: 'H6',hash: '',src: 'patterns/typography/h6.html',loadContext: '.page-h6'}
-							]
-						},
-						{
-							id: 'listestypo'
-							,title: 'Les listes'
-							,hash: ''
-						},
-						{
-							id: 'citetypo'
-							,title: 'Citation'
-							,hash: ''
-						},
-						{
-							id: 'tabletypo'
-							,title: 'Tableaux'
-							,hash: ''
-						},
-						{
-							id: 'autretypo'
-							,title: 'Autres balises'
-							,hash: ''
-						}
-					]
+				var $blocPattern = $contener.children('#' + self.id + '.panel-page');
 
-				},
-				{
-					id: 'layout'
-					,title: 'Layout'
-					,hash: ''
-					,patterns: [
-						{id: 'layout-simple',title: 'Layout simple',hash: '',src: '',loadContext: ''},
-						{id: 'off-canvas',title: 'Off canvas', hash: '', src: '', loadContext: ''},
-						{id: 'grid',title: 'Grille', hash: '', src: '', loadContext: ''},
-						{id: 'grid-block',title: 'Bloc de Grille', hash: '', src: '', loadContext: ''},
-						{id: 'panel', title: 'Panels', hash: '', src: '', loadContext: ''},
-						{id: 'tabs', title: 'Tabs', hash: '', src: '', loadContext: ''}
-					]
-				},
-				{
-					id: 'ui'
-					,title: 'Interface utilisateur'
-					,hash: ''
-					,patterns: [
-						{id: 'Navigation', title: '', hash: '', src: '', loadContext: ''},
-						{id: 'Boutons', title: '', hash: '', src: '', loadContext: ''},
-						{id: 'Tooltips', title: '', hash: '', src: '', loadContext: ''}
-					]
-				},
-				{
-					id: 'forms'
-					,title: 'Formulaires'
-					,hash: ''
-					,patterns: [{id: 'Alert box', title: '', hash: '', src: '', loadContext: ''}]
-				},
-				{
-					id: 'fx'
-					,title: 'Effets'
-					,hash: ''
-					,patterns: [{id: 'Masque', title: '', hash: '', src: '', loadContext: ''}]
+				if ( i == 0 ) $blocPattern.addClass('default');
+
+				$blocPattern.append('<h2><span>'+self.titlePage+'</span></h2>');
+
+				if ( self.patterns.length > 0 ) {
+					$.each(self.patterns, function(i){
+						$blocPattern.append('<div class="block-section" id="' + this.id + '"></div>');
+
+						$('#' + this.id).patternLoader(
+							{
+								file: this.src
+								,loadContext: this.loadContext
+								,code: this.code
+							}
+						);
+					});
+
 				}
-			]
-		};
-		//console.log(guiStructure);
+			});
 
-		var tplNav = $('#tpl-nav').html();
-		var output = Handlebars.compile(tplNav);
+			$navigation.affichePanelPage().navigation();
 
-		$('nav[role="navigation"]').append(output(guiStructure)).affichePanelPage().navigation();
+		});
 
 	}
 );
--- js/patternLoader.js
+++ js/patternLoader.js
@@ -1,7 +1,7 @@
 /**
  * Module de chargement de pattern
  */
-define(['jquery','handlebars'], function($,Handlebars){
+define(['jquery','handlebars','setting'], function($, Handlebars, setting){
 
 	$.fn.patternLoader = function (opts, myCcallback) {
 		// comment on valide que le type est objet
@@ -17,53 +17,53 @@ define(['jquery','handlebars'], function($,Handlebars){
 		return this.each(function(){
 
 			var $el = $(this);
-			
-			$.get(options.file, {}, function(data){
+			var file = options.file;
+			$.get(file, {}, function(data){
 				
-				var pattern = {
-					example : $(data).filter(options.loadContext).html()
-				};
-				var template = $('#tpl-tab').html();
-				console.log(template);
-
-
-				//console.log(pattern.exemple);
-				if (options.code) {
+				// on recupere la partie du code de la page qui nous interesse
+				var page  = $(data).filter(options.loadContext);
+				console.log('Page => '+file +' : '+page);
+				
+				var exemple = setting.panelPage;
+				if (setting.panelPage.patterns.code) {
+					var codePattern = [];
+					// on charge le template
+					var template = $('#tpl-tab').html();
 					var output = Handlebars.compile(template);
-					var output2 = output(pattern);
-					//console.log(pattern.example);
 
+					$(page).find('.pattern').each(function(i) {
+						codePattern[i] = $(this).html();
+						console.log('codePattern['+i+'] => ' + codePattern[i]);
+					});
+				
+						
+					//var output2 = output(pattern);
+				//var pattern = $(page).find('.pattern').html();
+				//console.log($(page).find('.pattern').html());
+					//$('section.pattern', page).each(function(index){
+						//$(this).replaceWith(output($(this).html()));
+						//console.log('Code['+index+'] ==> '+this);
+					//});
 
-					$el.append(output2)
-					//.find("pre.htmlCode")
-					//.snippet(
-					//	"html"
-					//	//, {style:"acid",transparent:true}
-					//)
-					;
-					//console.log(pattern);
+					//$el.append(output2)
+						//.find("pre.htmlCode").snippet("html", {style:"acid",transparent:true})
+					//;
+					//console.log(output2);
 				} else {
-					$el.append(pattern.example);
-					//console.log(pattern);
+					$el.append(page);
 				}
-				
-				
+
 				if (typeof myCcallback === 'function') {
 					myCcallback.call(this);
 				}
-
 			},"html")
 			/*.fail(function(data){
-				
 				console.log(data.status);
 				console.log(data.statusText);
-				
 				var msg = "Impossible de charger le fichier" + data.status + " " + data.statusText;
 				$('<div id="error"/>').insertAfter(this).html(msg);
 			})*/
 			;
-
 		});
-
 	};
 });
