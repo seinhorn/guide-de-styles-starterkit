aa09f7c7216a29343fce721d7f445f172a9473f0
acariou <acariou@figarocms.fr>
2013-12-16 09:16:59 +0100

    Refactor reformatage js

--- js/main.js
+++ js/main.js
@@ -4,26 +4,26 @@
 requirejs.config({
 
 	paths: {
-		'jquery': 		'vendor/jquery/1.8.3/jquery'
-		,'jquery.ui.core' : 'vendor/jquery-ui/jquery.ui.core'
-		,'jquery.ui.widget' : 'vendor/jquery-ui/jquery.ui.widget'
-		,'jquery.ui.tabs': 		'vendor/jquery-ui/jquery.ui.tabs'
-		,'h5f': 		'vendor/h5f/h5f.min'
-		,'snippet': 	'vendor/snippet/jquery.snippet.min'
-		,'handlebars': 	'vendor/handlebars/1.0.0/handlebars'
+		'jquery': 		'vendor/jquery/1.8.3/jquery',
+		'jquery.ui.core' : 'vendor/jquery-ui/jquery.ui.core',
+		'jquery.ui.widget' : 'vendor/jquery-ui/jquery.ui.widget',
+		'jquery.ui.tabs': 		'vendor/jquery-ui/jquery.ui.tabs',
+		'h5f': 		'vendor/h5f/h5f.min',
+		'snippet': 	'vendor/snippet/jquery.snippet.min',
+		'handlebars': 	'vendor/handlebars/1.0.0/handlebars'
 	},
 	shim: {
-		'jquery': { exports: '$' }
-		,'snippet': { deps: ['jquery'] }
-		,'affichePanelPage': { deps: ['jquery'] }
-		,'navigation': { deps: ['jquery'] }
-		,'alertBoxes': { deps: ['jquery'] }
-		, 'handlebars': {
+		'jquery': { exports: '$' },
+		'snippet': { deps: ['jquery'] },
+		'affichePanelPage': { deps: ['jquery'] },
+		'navigation': { deps: ['jquery'] },
+		'alertBoxes': { deps: ['jquery'] },
+		'handlebars': {
 			exports: 'Handlebars'
-		}
-		,'jquery.ui.core': { deps: ['jquery'] }
-		,'jquery.ui.widget': { deps: ['jquery.ui.core'] }
-		,'jquery.ui.tabs': { deps: [
+		},
+		'jquery.ui.core': { deps: ['jquery'] },
+		'jquery.ui.widget': { deps: ['jquery.ui.core'] },
+		'jquery.ui.tabs': { deps: [
 			'jquery',
 			'jquery.ui.core',
 			'jquery.ui.widget'
@@ -33,17 +33,16 @@ requirejs.config({
 
 require(
 	[
-		'setting'
-		,'jquery.ui.tabs'
-		//,'vendor/jquery-ui/jquery.ui.widget'
-		,'handlebars'
-		,'snippet'
-		,'alertBoxes'
-		,'patternLoader'
-		,'jquery'
-		,'h5f'
-		,'affichePanelPage'
-		,'navigation'
+		'setting',
+		'jquery.ui.tabs',
+		'handlebars',
+		'snippet',
+		'alertBoxes',
+		'patternLoader',
+		'jquery',
+		'h5f',
+		'affichePanelPage',
+		'navigation'
 	],
 	function (setting) {
 
@@ -89,11 +88,11 @@ require(
 
 						$('#' + this.id).patternLoader(
 							{
-								file: this.src
-								,loadContext: this.loadContext
-								,code: this.code
-							}
-							,function(){
+								file: this.src,
+								loadContext: this.loadContext,
+								code: this.code
+							},
+							function(){
 								$('.tabs').tabs();
 							}
 						);
